7841cf8763277b54c47f78208a83d1b0cbf454f3
jboszkiewicz <37620278+jboszkiewicz@users.noreply.github.com>
2018-05-28 18:04:51 +0200

    Update nowyGenerator.cpp

--- nowyGenerator.cpp
+++ nowyGenerator.cpp
@@ -31,3 +31,98 @@ void GenerateGraphMS(bool **macierz, int v, int e){
 		}
 	}
 }
+
+
+void GenerateGraphLN(std::list<int> *listaNastępników, int v, int e){
+	int i, x, y;
+	std::list<int>::iterator w;
+	i = 0;
+	while(i < e){
+		x = rand() % v;
+		y = rand() % v;
+		bool krawędźIstnieje = false;
+		for(w = listaNastępników[x].begin(); w != listaNastępników[x].end(); w++){
+			if(*w == y){
+				krawędźIstnieje = true;
+				break;
+			}
+		}
+		if(x != y && !krawędźIstnieje){
+			listaNastępników[x].push_back(y);
+			i++;
+		}
+	}
+
+	//modify to have EULER
+	int *stopnie = new int[v];
+	for(int i = 0; i < v; i++){
+		stopnie[i] = listaNastępników[i].size();
+	}
+	for(int i = 0; i < v; i++){
+		for(w = listaNastępników[i].begin(); w != listaNastępników[i].end(); w++){
+			stopnie[*w]--;
+		}
+	}
+	for(int i = 0; i<v - 1; i++){
+		//calculate degree
+		while(stopnie[i] != 0){
+			int x = rand() % (v - i - 1) + i + 1;
+			if(stopnie[i] > 0){//nadmiar wychodzących
+				if(stopnie[x] > 0)
+					continue;
+				bool krawędźIstnieje = false;//szukamy krawędzi z i do x
+				for(w = listaNastępników[i].begin(); w != listaNastępników[i].end(); w++){
+					if(*w == x){
+						krawędźIstnieje = true;
+						break;
+					}
+				}
+				if(krawędźIstnieje){//jeżeli krawędź istnieje - usuwamy
+					listaNastępników[i].erase(w);
+					stopnie[i]--;
+					stopnie[x]++;
+				} else{//jeżeli nie istnieje - szukamy odwrotnej
+					bool krawędźIstnieje = false;//szukamy krawędzi z x do i
+					for(w = listaNastępników[x].begin(); w != listaNastępników[x].end(); w++){
+						if(*w == i){
+							krawędźIstnieje = true;
+							break;
+						}
+					}
+					if(!krawędźIstnieje){//jeżeli nie istnieje - dodajemy ją
+						listaNastępników[x].push_back(i);
+						stopnie[i]--;
+						stopnie[x]++;
+					}
+				}
+			} else{//nadmiar wchodzących
+				if(stopnie[x] < 0)
+					continue;
+				bool krawędźIstnieje = false;//szukamy krawędzi z i do x
+				for(w = listaNastępników[i].begin(); w != listaNastępników[i].end(); w++){
+					if(*w == x){
+						krawędźIstnieje = true;
+						break;
+					}
+				}
+				if(!krawędźIstnieje){//jeżeli nie istnieje - dodajemy ją
+					listaNastępników[i].push_back(x);
+					stopnie[i]++;
+					stopnie[x]--;
+				} else{//jeżeli istnieje - szukamy odwrotnej
+					bool krawędźIstnieje = false;//szukamy krawędzi z x do i
+					for(w = listaNastępników[x].begin(); w != listaNastępników[x].end(); w++){
+						if(*w == i){
+							krawędźIstnieje = true;
+							break;
+						}
+					}if(krawędźIstnieje){//jeżeli krawędź istnieje - usuwamy
+						listaNastępników[x].erase(w);
+						stopnie[i]++;
+						stopnie[x]--;
+					}
+				}
+			}
+		}
+	}
+}
